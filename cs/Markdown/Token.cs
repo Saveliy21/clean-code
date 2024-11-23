0d90bbb5836f72ade1c07eb470d3628819abe938
Савелий <vildanov.s@bk.ru>
2024-11-24 00:07:31 +0500

    refactor Token.cs

--- cs/Markdown/Token.cs
+++ cs/Markdown/Token.cs
@@ -4,16 +4,14 @@ namespace Markdown;
 
 public class Token
 {
-    public bool IsTag { get; }
     public int StartIndex { get; }
     public int Length { get; }
 
     // Если Type = null, значит этот токен для текста, иначе - для тэга.
     public ITagsType? Type { get; }
 
-    public Token(bool isTag, int startIndex, int length, ITagsType? type = null)
+    public Token(int startIndex, int length, ITagsType? type = null)
     {
-        IsTag = isTag;
         StartIndex = startIndex;
         Length = length;
         Type = type;
